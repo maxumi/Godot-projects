bef144982a9d71b8f47f7e87617995c1513b8a45
Max Felding <maxjac64@gmail.com>
2025-02-26 08:23:35 +0100

    finished "your first 2d game"

--- first_2d_project/main.gd
+++ first_2d_project/main.gd
@@ -6,7 +6,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,21 +14,29 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_player_hit() -> void:
-	pass # Replace with function body.
+
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
+
 
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
